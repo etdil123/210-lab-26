34ac4f26f25ecb84aba66e00a2c9eb56b2b344dd
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-10-26 22:20:31 -0700

    10 min commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 // COMSC 210 | Ethan Dilk | Lab 25 Races
 #include <iostream>
 #include <chrono>
+#include <vector>
+#include <list>
+#include <set>
 using namespace std;
+using namespace std::chrono;
+
+
 
 int main() {
     
+    // initialize lists to store scores
+    list<int> readingTimes, sortingTimes, insertingTimes, deletingTimes;
+
 
     return 0;
 }
